6fea85e4976af214adc6c009f686763b380e5de7
jimanloi <jimanloi@hotmail.com>
2025-06-02 17:04:56 +0200

    added favorite button

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,11 +1,22 @@
 import type { Book } from "../types";
 import "./Books.css";
 import defaultBookCover from "../assets/defbookcover.jpg";
+import { useState } from "react";
+
+import { FaHeart } from "react-icons/fa6";
+import { FaRegHeart } from "react-icons/fa";
 
 const BookCard = ({ book }: { book: Book }) => {
+  const [favorite, setFavorite] = useState(false);
+
+  const favoriteHandler = () => {
+    setFavorite(!favorite);
+  };
   return (
     <div key={book.key} className="book-container">
-      <h5>{book.title}</h5>
+      <div className="book-title">
+        <h6>{book.title}</h6>
+      </div>
       <img
         src={
           book.cover_i
@@ -15,11 +26,33 @@ const BookCard = ({ book }: { book: Book }) => {
         alt={book.key}
         className="book-cover"
       />
-      <p>
-        {book.author_name?.join(", ") || "Unknown Author"}{" "}
+      <p className="book-description">
+        <span
+          style={{
+            maxWidth: "150px",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+          }}
+        >
+          {book.author_name[0] || "Unknown Author"}{" "}
+        </span>
         <span>
           <small>{book.first_publish_year}</small>
         </span>
+        <button
+          onClick={favoriteHandler}
+          style={{
+            background: "none",
+            color: "inherit",
+            border: "none",
+            padding: 0,
+            font: "inherit",
+            cursor: "pointer",
+            outline: "inherit",
+          }}
+        >
+          {favorite ? <FaHeart /> : <FaRegHeart />}
+        </button>
       </p>
     </div>
   );
